91751a0775dc624cef2648685c5caf0638c3dd89
Enea Xharja <19818937+eneax@users.noreply.github.com>
2021-07-29 21:21:45 +0200

    Create basic list component

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const ListScreen = () => {
+  return (
+    <View>
+      <Text>This is the list screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default ListScreen

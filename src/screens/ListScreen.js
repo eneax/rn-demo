80d0fedda19afa31ca16afabd2ddbf371a89b6e4
Enea Xharja <19818937+eneax@users.noreply.github.com>
2021-07-29 21:50:04 +0200

    Display FlatList

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,14 +1,43 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
 
 const ListScreen = () => {
+  const friends = [
+    { name: 'Friend #01' },
+    { name: 'Friend #02' },
+    { name: 'Friend #03' },
+    { name: 'Friend #04' },
+    { name: 'Friend #05' },
+    { name: 'Friend #06' },
+    { name: 'Friend #07' },
+    { name: 'Friend #08' },
+    { name: 'Friend #09' },
+    { name: 'Friend #10' },
+  ]
+
   return (
-    <View>
-      <Text>This is the list screen</Text>
-    </View>
+    <FlatList
+      keyExtractor={(friend) => friend.name}
+      data={friends}
+      renderItem={({ item }) => (
+        <View>
+          <Text>{item.name}</Text>
+        </View>
+      )}
+    />
   )
 }
 
 const styles = StyleSheet.create({})
 
 export default ListScreen
+
+/*
+! FlatList
+
+- 2 required props: data and renderItem
+- data: the array of data that we are going to create a bunch of elements out of
+- renderItem: function that will turn each individual item into an element
+- keyExtractor: returns a unique string as a key for every element
+
+*/

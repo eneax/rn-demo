bf6c30654c76f8416ccb69d43218ac6f130b06ed
Enea Xharja <19818937+eneax@users.noreply.github.com>
2021-07-31 13:43:51 +0200

    Communicating from Parent to Child

---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+import ImageDetail from '../components/ImageDetail'
+
+const ImageScreen = () => {
+  return (
+    <View>
+      <ImageDetail title="Forest" />
+      <ImageDetail title="Beach" />
+      <ImageDetail title="Mountain" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default ImageScreen

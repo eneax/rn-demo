10c17f0dbb627499378a935d970dac33c136eb1a
Enea Xharja <19818937+eneax@users.noreply.github.com>
2021-08-01 11:21:57 +0200

    Build counter with useState

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -2,9 +2,19 @@ import React from 'react'
 import { View, Text, Button, StyleSheet } from 'react-native'
 
 const CounterScreen = () => {
+  const [counter, setCounter] = React.useState(0)
+
   return (
     <View>
-      <Text>Current count: </Text>
+      <Button
+        title="Increase counter"
+        onPress={() => setCounter(counter + 1)}
+      />
+      <Button
+        title="Decrease counter"
+        onPress={() => setCounter(counter - 1)}
+      />
+      <Text>Current Count: {counter}</Text>
     </View>
   )
 }

27b905fa4ce2a8e66e29e1417b45796496181f9d
Enea Xharja <19818937+eneax@users.noreply.github.com>
2021-08-07 11:45:58 +0200

    Re-write CounterScreen with useReducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,3 +1,4 @@
+// ! With useState
 import React from 'react'
 import { View, Text, Button, StyleSheet } from 'react-native'
 
@@ -22,3 +23,43 @@ const CounterScreen = () => {
 const styles = StyleSheet.create({})
 
 export default CounterScreen
+
+// ! With useReducer
+// import React from 'react'
+// import { View, Text, Button, StyleSheet } from 'react-native'
+
+// const reducer = (state, action) => {
+//   // state === { count: number }
+//   // action === { type: 'increment' || 'decrement', payload: 1 }
+
+//   switch (action.type) {
+//     case 'increment':
+//       return { ...state, count: state.count + action.payload }
+//     case 'decrement':
+//       return { ...state, count: state.count - action.payload }
+//     default:
+//       return
+//   }
+// }
+
+// const CounterScreen = () => {
+//   const [state, dispatch] = React.useReducer(reducer, { count: 0 })
+
+//   return (
+//     <View>
+//       <Button
+//         title="Increase counter"
+//         onPress={() => dispatch({ type: 'increment', payload: 1 })}
+//       />
+//       <Button
+//         title="Decrease counter"
+//         onPress={() => dispatch({ type: 'decrement', payload: 1 })}
+//       />
+//       <Text>Current Count: {state.count}</Text>
+//     </View>
+//   )
+// }
+
+// const styles = StyleSheet.create({})
+
+// export default CounterScreen

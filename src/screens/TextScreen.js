073fe4f70e6689da6b74ece4ba692a0e09841747
Enea Xharja <19818937+eneax@users.noreply.github.com>
2021-08-07 13:55:27 +0200

    Update TextInput state with onChangeText

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,13 +2,19 @@ import React from 'react'
 import { View, Text, TextInput, StyleSheet } from 'react-native'
 
 const TextScreen = () => {
+  const [name, setName] = React.useState('')
+
   return (
     <View>
+      <Text style={styles.text}>Enter name:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
+        value={name}
+        onChangeText={(newValue) => setName(newValue)}
       />
+      <Text style={styles.text}>My name is {name}</Text>
     </View>
   )
 }
@@ -19,6 +25,9 @@ const styles = StyleSheet.create({
     padding: 15,
     borderColor: 'black',
     borderWidth: 1,
+  },
+  text: {
+    margin: 15,
   },
 })
 

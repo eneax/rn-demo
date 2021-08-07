f6abad2050624905c3371e374ee7666f392cf01b
Enea Xharja <19818937+eneax@users.noreply.github.com>
2021-08-07 11:58:55 +0200

    Style TextInput

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,21 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
 
 const TextScreen = () => {
   return (
     <View>
-      <Text>Text Screen</Text>
+      <TextInput style={styles.input} />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    padding: 15,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+})
 
 export default TextScreen
